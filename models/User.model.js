55c759ebeaa85a5550af19d81888e9b1434540b4
Camilo Espinosa <espinosacamilop1@gmail.com>
2021-09-04 17:20:32 -0400

    login, signup, dashboard  were all set up, just a couple tweaks to be done

--- models/User.model.js
+++ models/User.model.js
@@ -2,6 +2,9 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
   username: {
     type: String,
     // unique: true -> Ideally, should be unique, but its up to you
