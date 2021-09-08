bb76415978e9ab40c0eb31b3a8977ebc22221fb9
ricardofer2412 <richardcasanovac@gmail.com>
2021-09-07 21:33:06 -0400

    fix routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,41 +11,24 @@ router.get("/signup", isLoggedOut, (req, res, next) => {
 router.post("/signup", isLoggedOut, (req, res, next) => {
   const { firstName, lastName, email, username, password } = req.body;
 
-    if(!firstName || !lastName || !email || !username || !password){
-        res.render('user/signup', {
-        errorMessage: "Please fill out all required fields"
-        })
-    }else{
-       User.findOne({username}).then(user =>{
-            console.log("user from db", username , email);
-            if(!user){
-            const hashedPassword = bcrypt.hashSync(password, 10)
-             User.create({
-                firstName,
-                lastName,
-                email,
-                username,
-                password: hashedPassword
-                
-            }).then(createdUser =>{
-                req.session.currentUser = createdUser;
-            res.redirect('/dashboard');
-            })
-        }
-       })
-    }
-})
-
-router.get('/login', isLoggedOut, (req, res, next) =>{
-    res.render('user/login')
-})
-
-router.post('/login', isLoggedOut, (req, res, next) =>{
-    console.log('SESSION ======>', req.session)
-    const {username, password} = req.body;
-    if(!username || !password){
-        res.render('user/login', {
-            errorMessage: "Please enter both username and password"
+  if (!firstName || !lastName || !email || !username || !password) {
+    res.render("user/signup", {
+      errorMessage: "Please fill out all required fields",
+    });
+  } else {
+    User.findOne({ username }).then((user) => {
+      console.log("user from db", username, email);
+      if (!user) {
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        User.create({
+          firstName,
+          lastName,
+          email,
+          username,
+          password: hashedPassword,
+        }).then((createdUser) => {
+          req.session.currentUser = createdUser;
+          res.redirect("/dashboard");
         });
       }
     });
@@ -84,12 +67,12 @@ router.post("/login", isLoggedOut, (req, res, next) => {
 
 //this is logout routes
 
-router.post('/logout', isLoggedIn, (req, res, next) => {
-    req.session.destroy(err => {
-        if(err) next(err);
-        res.redirect('/');
-    })
-})
+router.post("/logout", isLoggedIn, (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
+  });
+});
 
 router.get("/profile", isLoggedIn, (req, res, next) => {
   console.log("user in session:", { userInSession: req.session.currentUser });
