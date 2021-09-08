468d6fec8cbf50388fc38db0ea922acef8a270c2
ricardofer2412 <richardcasanovac@gmail.com>
2021-09-08 06:26:09 -0400

    fix one on one relation user account

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/dashboard", isLoggedIn, (req, res, next) => {
+  res.render("dashboard/dashboard");
 });
 
 module.exports = router;
