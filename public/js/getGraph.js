32496e66710461bfc87ff4e9413ce7006a37782d
ricardofer2412 <richardcasanovac@gmail.com>
2021-09-15 15:03:21 -0400

    add graph

---
+++ public/js/getGraph.js
@@ -0,0 +1,46 @@
+function getGraph() {
+  console.log("This is graph");
+}
+window.onload = () => {
+  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo`;
+  axios.get(url).then((responseFromAPI) => {
+    console.log(responseFromAPI);
+    const timeSeries = responseFromAPI.data["Time Series (5min)"];
+
+    const labels = Object.keys(timeSeries);
+
+    const prices = labels.map((label) => timeSeries[label]["4. close"]);
+    console.log(labels, prices);
+
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Stock Prices",
+            data: prices,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+  });
+};
