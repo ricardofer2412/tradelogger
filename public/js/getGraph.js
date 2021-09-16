4619b5814e522c7c7243b91e7093f31fb9ccff18
ricardofer2412 <richardcasanovac@gmail.com>
2021-09-16 14:51:13 -0400

    trying to add the graph

--- public/js/getGraph.js
+++ public/js/getGraph.js
@@ -1,44 +1,56 @@
+const { default: axios } = require("axios");
 
-const canvas = document.getElementById('myChart')
-const ctx = canvas.getContext('2d');
+// const canvas = document.getElementById("myChart");
+// const ctx = canvas.getContext("2d");
 
-const finnhub = require('finnhub');
-const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = "budf5mn48v6ped90n62g" // Replace this
-const finnhubClient = new finnhub.DefaultApi()
+// function drawGraph() {
+//   var myChart = new Chart(ctx, {
+//     type: "bar",
+//     data: {
+//       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//       datasets: [
+//         {
+//           label: "# of Votes",
+//           data: [12, 19, 3, 5, 2, 3],
+//           backgroundColor: [
+//             "rgba(255, 99, 132, 0.2)",
+//             "rgba(54, 162, 235, 0.2)",
+//             "rgba(255, 206, 86, 0.2)",
+//             "rgba(75, 192, 192, 0.2)",
+//             "rgba(153, 102, 255, 0.2)",
+//             "rgba(255, 159, 64, 0.2)",
+//           ],
+//           borderColor: [
+//             "rgba(255, 99, 132, 1)",
+//             "rgba(54, 162, 235, 1)",
+//             "rgba(255, 206, 86, 1)",
+//             "rgba(75, 192, 192, 1)",
+//             "rgba(153, 102, 255, 1)",
+//             "rgba(255, 159, 64, 1)",
+//           ],
+//           borderWidth: 1,
+//         },
+//       ],
+//     },
+//     options: {
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//         },
+//       },
+//     },
+//   });
+// }
 
+const apiKey = "RZHOBZ5MUSOW2S61";
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+async function getPrices() {
+  console.log("get prices");
+  const res = await axios.get(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${apiKey}`
+  );
+  console.log(res);
+}
+// drawGraph();
+
+getPrices();
