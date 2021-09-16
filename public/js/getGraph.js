c9ed19a9a8645e7c505b6288a38ce2621cb83682
Mac <ricardofer2412@hotmail.com>
2021-09-16 17:07:36 -0400

    graph working

--- public/js/getGraph.js
+++ public/js/getGraph.js
@@ -1,44 +1,47 @@
-
-const canvas = document.getElementById('myChart')
-const ctx = canvas.getContext('2d');
-
-const finnhub = require('finnhub');
-const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = "budf5mn48v6ped90n62g" // Replace this
-const finnhubClient = new finnhub.DefaultApi()
-
-
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+window.onload = () => {
+    
+    const ticker =  document.getElementById('ticker').innerText
+    console.log('this is ticker', ticker)
+    const api = 'RZHOBZ5MUSOW2S61'
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${api}`;
+    axios.get(url).then((responseFromAPI) => {
+      console.log(responseFromAPI);
+      const timeSeries = responseFromAPI.data["Time Series (5min)"];
+  
+      const labels = Object.keys(timeSeries);
+  
+      const prices = labels.map((label) => timeSeries[label]["4. close"]);
+      console.log(labels, prices);
+  
+      const ctx = document.getElementById("myChart").getContext("2d");
+      const myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels,
+          datasets: [
+            {
+              label: "Stock Prices",
+              data: prices,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "green",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+      });
+    });
+  };
