c45a0fd637e5f58bf53c7bead7b407a002ed3811
David Kai Landes <davidlandes24@gmail.com>
2021-09-18 17:17:31 -0400

    mailer started

---
+++ routes/mailer.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+async function main() {
+
+router.post('/send-email', (req, res, next) => {
+    let { email, subject, message } = req.body;
+    let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+      }
+    });
+    transporter.sendMail({
+      from: '"My Awesome Project " <kingtrader.usa@gmail.com>',
+      to: email, 
+      subject: 'Awesome Subject',
+      text: message,
+      html: `<b>${message}</b>`
+    })
+    .then(info => res.render('message', {email, subject, message, info}))
+    .catch(error => console.log(error));
+  });
+
+}
