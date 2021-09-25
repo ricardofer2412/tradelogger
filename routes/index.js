0cc23adc40a9c9214db5f70a6e906ff3463718f8
ricardofer2412 <richardcasanovac@gmail.com>
2021-09-25 07:55:38 -0400

    added watch list remove

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
 const Account = require("../models/Account.model");
 const finnhub = require("finnhub");
 const Trade = require("../models/Trade.model");
-const Watchlist = require('../models/Watchlist.model')
+const Watchlist = require("../models/Watchlist.model");
 
 const api_key = finnhub.ApiClient.instance.authentications["api_key"];
 api_key.apiKey = "";
@@ -18,46 +18,53 @@ router.get("/", isLoggedIn, (req, res, next) => {
   res.render("dashboard/dashboard");
 });
 
+router.post("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  Watchlist.findByIdAndDelete(id).then(() => {
+    console.log("Ticker remove from Watchlist");
+    res.redirect("back");
+  });
+});
+
 router.get("/dashboard", isLoggedIn, (req, res, next) => {
   const user = req.session.currentUser._id;
 
   Account.find({ userId: { $eq: user } }).then((account) => {
     const accountId = account[0]._id;
-    console.log(accountId);
     Trade.find({ accountId: { $eq: accountId } }).then((trade) => {
-      console.log("This is trade ", trade);
       let sumBalance = 0;
       for (let i = 0; i < trade.length; i++) {
         sumBalance = trade[i].tradeValue + sumBalance;
       }
-      const userId = req.session.currentUser._id
-     
-      Watchlist.find({authorId: userId}).then((watchList) => {
-        
-        let newAccountBalance = sumBalance + account[0].buyingPower;            
+      const userId = req.session.currentUser._id;
+
+      Watchlist.find({ authorId: userId }).then((watchList) => {
+        let newAccountBalance = sumBalance + account[0].buyingPower;
         const accountInfo = account[0];
-        console.log('watchlist', watchList)
-        const arrOfObj = []
+        const arrOfObj = [];
         for (let i = 0; i < watchList.length; i++) {
-        const ticker = watchList[i].tickerId
+          const ticker = watchList[i].tickerId;
           finnhubClient.quote(ticker, (error, data, response) => {
-            const testArr = []
+            const testArr = [];
             let prices = data.c;
-            console.log('data',data.c)
-            arrOfObj.push({ticker: ticker, price: prices})
+
+            arrOfObj.push({ ticker: ticker, price: prices });
             // console.log(ticker)
-            console.log(testArr)
-          })
-        };
-         console.log('this is arra of obj',arrOfObj)      
+          });
+        }
         // Promise.all(arrOfObj).then()
-          
-      finnhubClient.marketNews("general", {}, (error, data, response) => {
-        const news = data;
-            
-        res.render("dashboard/dashboard", { accountInfo, news, trade, watchList});
-            })
-              
+
+        finnhubClient.marketNews("general", {}, (error, data, response) => {
+          const news = data;
+
+          res.render("dashboard/dashboard", {
+            accountInfo,
+            news,
+            trade,
+            watchList,
+          });
+        });
       });
       // console.log("this is sum", sumBalance);
       // console.log("This is new Balance", newAccountBalance);
